ad1ac318d87a16aab0b55e3ccd238c769b1f3e0a
Daniel Harvey <daniel.harvey@consbio.org>
2016-09-08 13:56:59 -0700

    Tag increment and new keyword.

--- setup.py
+++ setup.py
@@ -21,8 +21,8 @@ class RunTests(Command):
 setup(
     name='gis_metadata_parser',
     description='Parser for GIS metadata standards including FGDC and ISO-19115',
-    keywords='fgdc,iso,ISO-19115,metadata,xml,parser',
-    version='0.2.3',
+    keywords='fgdc,iso,ISO-19115,ISO-19139,metadata,xml,parser',
+    version='0.3.0',
     packages=[
         'gis_metadata', 'gis_metadata.tests'
     ],
