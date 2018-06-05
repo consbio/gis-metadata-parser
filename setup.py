bf97132fd263026aea42d5af9772d378eaec67d9
dharvey-consbio <daniel.harvey@consbio.org>
2018-06-05 16:13:15 -0700

    Enable markdown for PyPI README

--- setup.py
+++ setup.py
@@ -26,8 +26,9 @@ setup(
     name='gis_metadata_parser',
     description='Parser for GIS metadata standards including FGDC and ISO-19115',
     long_description=long_description,
+    long_description_content_type='text/markdown',
     keywords='arcgis,fgdc,iso,ISO-19115,ISO-19139,gis,metadata,parser,xml,gis_metadata,gis_metadata_parser',
-    version='1.1.2',
+    version='1.1.3',
     packages=[
         'gis_metadata', 'gis_metadata.tests'
     ],
